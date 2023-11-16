c59ee3a7c08c937f864fa7436d923156e1fddbf4
marcebenitez2 <marcebenitez0607@gmail.com>
2023-11-16 11:50:41 -0300

    Update navbar name width to fit longer text

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -21,7 +21,7 @@ function Navbar() {
   return (
     <div className="w-full h-16 flex items-center justify-between text-white">
       <div className="flex items-center gap-2 smn:gap-2">
-        <h1 className="text-3xl navbar_name w-48 animate-fade-down animate-delay-300 smn:text-lg smn:w-fit">
+        <h1 className="text-3xl navbar_name w-60 animate-fade-down animate-delay-300 smn:text-lg smn:w-fit">
           OPEN TO WORK
         </h1>
         <div className="w-3 h-3 bg-green-400 rounded-full animate-pulse animate-infinite"></div>
