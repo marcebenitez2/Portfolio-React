995395c9c78efb7ca1b26eba1eef460d76885b2a
marcebenitez2 <marcebenitez0607@gmail.com>
2023-11-16 17:28:04 -0300

    Update CSS styles for better responsiveness

--- src/components/footerHero.jsx
+++ src/components/footerHero.jsx
@@ -3,7 +3,7 @@ import logo from "../assets/logoCircular.png";
 function FooterHero() {
   return (
     <div>
-      <img src={logo} className="absolute w-28 left-4 bottom-8 logo smn:w-20" />
+      <img src={logo} className=" logo absolute w-28 left-4 bottom-8 logo smn:w-20" />
       <div className="absolute right-6 text-white bottom-1 text-lg text-right">
         <h3 className="font-semibold text-orange-400"><a href="#contacto">Hablemos!</a></h3>
         <h3 className="font-semibold">En busqueda laboral</h3>
--- src/components/title.jsx
+++ src/components/title.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Title() {
   return (
-    <div className="w-full py-20 flex flex-col text-[#D1D5D8] leading-none">
+    <div className="container-title w-full py-20 flex flex-col text-[#D1D5D8] leading-none">
       <div className="flex items-center">
         <div className="title_hero hover:text-orange-400">FULL</div>
         <div className="title_hero hover:text-orange-400">STACK</div>
